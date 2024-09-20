970704173097f02e4237f79ffed2fd27f57027a4
Ravindu Deshan <Droidcasts@protonmail.com>
2024-09-20 05:49:23 +0000

    Introduced a simple build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+RDIR="$(pwd)"
 
+#OEM variabls
 export ARCH=arm64
 export PLATFORM_VERSION=12
 export ANDROID_MAJOR_VERSION=s
 
-make ARCH=arm64 exynos9820-beyondxks_defconfig
-make ARCH=arm64 -j16
+#main variables
+export ARGS="
+-j$(nproc)
+ARCH=arm64
+CLANG_TRIPLE=${RDIR}/toolchain/clang/host/linux-x86/clang-4639204-cfp-jopp/bin/aarch64-linux-gnu-
+CROSS_COMPILE=${RDIR}/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+CC=${RDIR}/toolchain/clang/host/linux-x86/clang-4639204-cfp-jopp/bin/clang
+"
+#building function
+build(){
+    make ${ARGS} exynos9820-beyondxks_defconfig
+    make ${ARGS} menuconfig
+    make ${ARGS}
+}
+
+build
