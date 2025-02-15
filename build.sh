e8f41ebc8a456e38a0351e23109b147075866944
Ravindu Deshan <Droidcasts@protonmail.com>
2025-02-15 05:21:35 +0000

    build script: added a custom nethunter defconfig

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ CC=${RDIR}/toolchain/clang/host/linux-x86/clang-4639204-cfp-jopp/bin/clang
 
 #building function
 build_ksu(){
-    make ${ARGS} exynos9820-beyond2lte_defconfig beyond2.config ksu.config version.config
+    make ${ARGS} exynos9820-beyond2lte_defconfig beyond2.config ksu.config version.config nethunter.config
     make ${ARGS} menuconfig || true
     make ${ARGS} || exit 1
 }
