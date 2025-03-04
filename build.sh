6c15aea0962bdc11cf55e94edabcd20c67014c46
Ravindu Deshan <Droidcasts@protonmail.com>
2025-03-04 06:55:56 +0000

    unified kernel: part 1
    zip -> tar
    
    don't clear the terminal
    
    Signed-off-by: Ravindu Deshan <Droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 RDIR="$(pwd)"
 export KBUILD_BUILD_USER="@ravindu644"
+export MODEL=$1
 
 #build directory
 rm -rf ${RDIR}/build && mkdir -p ${RDIR}/build
+
+# Device configuration
+declare -A DEVICES=(
+    [beyond2]="exynos9820-beyond2_defconfig 9820 SRPRI17C014KU"
+    [beyond1]="exynos9820-beyond1_defconfig 9820 SRPRI28B014KU"
+    [beyond0]="exynos9820-beyond0_defconfig 9820 SRPRI28A014KU"
+    [beyondxks]="exynos9820-beyondxks_defconfig 9820 SRPSC04B011KU"
+)
+
+# Set device-specific variables
+if [[ -v DEVICES[$MODEL] ]]; then
+    read KERNEL_DEFCONFIG SOC BOARD <<< "${DEVICES[$MODEL]}"
+else
+    echo "Unknown device: $MODEL, setting to beyondxks"
+    export MODEL="beyondxks"
+    read KERNEL_DEFCONFIG SOC BOARD <<< "${DEVICES[beyondxks]}"
+fi
 
 #kernelversion
 if [ -z "$BUILD_KERNEL_VERSION" ]; then
@@ -29,7 +47,7 @@ CC=${RDIR}/toolchain/clang/host/linux-x86/clang-4639204-cfp-jopp/bin/clang
 
 #building function
 build_kernel(){
-    make ${ARGS} exynos9820-beyondxks_defconfig common.config version.config
+    make ${ARGS} "${KERNEL_DEFCONFIG}" common.config version.config
     make ${ARGS} menuconfig || true
     make ${ARGS} || exit 1
 }
@@ -38,6 +56,7 @@ build_kernel(){
 build_boot() {    
     rm -f ${RDIR}/AIK-Linux/split_img/boot.img-kernel ${RDIR}/AIK-Linux/boot.img
     cp "${RDIR}/arch/arm64/boot/Image" ${RDIR}/AIK-Linux/split_img/boot.img-kernel
+    echo $BOARD > ${RDIR}/AIK-Linux/split_img/boot.img-board
     mkdir -p ${RDIR}/AIK-Linux/ramdisk
     cd ${RDIR}/AIK-Linux && ./repackimg.sh --nosudo && mv image-new.img ${RDIR}/build/boot.img
 }
@@ -45,7 +64,7 @@ build_boot() {
 #build odin flashable tar
 build_tar(){
     cp ${RDIR}/prebuilt-images/* ${RDIR}/build && cd ${RDIR}/build
-    tar -cvf "Kernel-SM-G977N-${BUILD_KERNEL_VERSION}.tar" boot.img dt.img.lz4 && rm boot.img dt.img.lz4
+    tar -cvf "Kernel-${MODEL}-${BUILD_KERNEL_VERSION}-stock-One-UI.tar" boot.img dt.img.lz4 && rm boot.img dt.img.lz4
     echo -e "\n[i] Build Finished..!\n" && cd ${RDIR}
 }
 
